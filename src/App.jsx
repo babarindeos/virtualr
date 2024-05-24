c4fdbf5b6089073bceac1e2b822be8350d873f64
Oluwaseyi Babarinde <babarindeos@funaab.edu.ng>
2024-05-24 18:34:49 +0100

    Workflow Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Navbar  from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import FeatureSection from './components/FeatureSection';
+import Workflow from './components/Workflow';
 
 export const App = () => {
   return (
@@ -11,6 +12,7 @@ export const App = () => {
        <div className="max-w-7xl border border-0 mx-auto py-4">
               <HeroSection/>
               <FeatureSection/>
+              <Workflow />
        </div>
        
     </div>
