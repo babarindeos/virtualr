61c3c5a7325fdb52bf3c21b5fbddd0b641efb06e
Oluwaseyi Babarinde <babarindeos@funaab.edu.ng>
2024-05-24 14:28:36 +0100

    Hero Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 
 import React from 'react'
 import Navbar  from './components/Navbar';
+import HeroSection from './components/HeroSection';
 
 export const App = () => {
   return (
     <div>
        <Navbar />
+       <div className="max-w-7xl border border-0 mx-auto py-4">
+              <HeroSection/>
+       </div>
+       
     </div>
   )
 }
